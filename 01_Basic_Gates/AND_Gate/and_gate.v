326e52d2f1ad777f253aad85eea15f461ebab7c0
Venkatesh Otturi <otturivenkatesh@gmail.com>
2026-06-20 11:43:58 +0530

    Update and_gate.v

--- 01_Basic_Gates/AND_Gate/and_gate.v
+++ 01_Basic_Gates/AND_Gate/and_gate.v
@@ -1,5 +1,25 @@
+/*
+//behavioural modelling
+module and_gate(a,b,y);
+  input a,b;
+  output reg y;
+  always @(*)begin
+    y = a & b;
+  end
+endmodule
+*/
+//data flow modelling
 module and_gate(a,b,y);
 input a,b;
 output y;
 assign y= a & b;
 endmodule 
+
+/*
+//gate level modelling
+module and_gate_gatelevel(a,b,y);
+  input a,b;
+  output y;
+  and g1 (y,a,b);
+endmodule
+*/
